1cc216bc7c026921037852eb843303b92b7eb81d
roshni73 <roshani.poudel@togglecorp.com>
2025-07-14 16:58:44 +0545

    fixup! fix: fix ci.yml

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: '/backend/schema.graphql',
+    schema: './backend/schema.graphql',
     documents: [
         'src/**/*.tsx',
         'src/**/*.ts',
